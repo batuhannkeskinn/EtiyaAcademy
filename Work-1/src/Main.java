dd225a516eb3b7795b4d4570723142baa1488ea0
Batuhan Keskin <77542685+batuhannkeskinn@users.noreply.github.com>
2022-11-19 17:42:12 +0300

    Update Main.java

--- Work-1/src/Main.java
+++ Work-1/src/Main.java
@@ -2,12 +2,22 @@ public class Main {
     public static void main(String[] args) {
     CustomerManager customerManager = new CustomerManager(new ICustomerMemoryDal());
     ICustomerMemoryDal iCustomerMemoryDal = new ICustomerMemoryDal();
-   // customerManager.add(new Customer(5,"","erdog",21));
+    //-----------------------------ADD---------------------------------------------------------------
+    customerManager.add(new Customer(5,"Eren","Erdoğan",21));
+    //--------------------------------GetAll----------------------------------------------------------
 
-     //customerManager.getById(2);
         customerManager.getAll();
+    //----------------------------------Update-------------------------------------------
        Customer customer = new Customer(2,"SS","BBB",15);
         customerManager.update(3,customer);
+    //-----------------------------------Delete----------------------------------------------
+        Customer customer2 = new Customer(2,"SS","BBB",15);
+        customerManager.delete(customer2);
         customerManager.getAll();
+    //-----------------------------------GetById----------------------------------------------------
+        customerManager.getById(2);
+
     }
+
+
 }
